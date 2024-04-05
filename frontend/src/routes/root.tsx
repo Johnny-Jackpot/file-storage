69c3630d8feecb60158046276fd8b5f479350cf0
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-04-05 19:32:30 +0300

    Global pending UI

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -1,4 +1,4 @@
-import {Outlet, useLoaderData, Form, redirect, NavLink} from "react-router-dom";
+import {Outlet, useLoaderData, Form, redirect, NavLink, useNavigation} from "react-router-dom";
 import {getContacts, createContact} from "../contacts.ts";
 
 export async function action() {
@@ -13,6 +13,7 @@ export async function loader() {
 
 export default function Root() {
   const {contacts} = useLoaderData();
+  const navigation = useNavigation();
 
   return (
     <>
@@ -75,7 +76,7 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail" className={navigation.state === "loading" ? "loading" : ""}>
         <Outlet/>
       </div>
     </>
